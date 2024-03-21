77f85d7121cebeae5ca917fae4af042337ba3275
Ashok Reddy Kolli <148216864+lonasic@users.noreply.github.com>
2024-03-21 18:18:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ default = ["user1", "user2", "user3", "user4"]
 }
 
 resource "aws_ebs_volume" "eight" {
- availability_zone = "ap-southeast-1a"
+ availability_zone = "ap-south-1a"
   size = 40
   tags = {
     Name = "ebs-001"
